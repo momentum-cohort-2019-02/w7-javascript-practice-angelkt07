acc22f6e2f23f0bae3de7c576a04e3e878e7ae11
Kasey Tiller <kntiller@ccicustom.com>
2019-03-25 13:27:34 -0400

    Completed 1-3

--- homework.js
+++ homework.js
@@ -1,14 +1,28 @@
 // 1. Set the variable `givenName` to the string "Addison".
 
+const givenName = "Addison"
+
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
+const candies = 20
+const people = 6
+const leftover = (candies%people)
 
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
 
+function greeting(name) {
+    return `Hello, ${name}!`
+}
+
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
+
+
+function isOdd(number) {
+    return number % 2 === 1 || number % 2 === -1
+}
 
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
